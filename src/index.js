4c22884dceb3dd9227c722440b1188c1bf719951
Nishant kumar <nishant927472@gmail.com>
2025-04-16 17:54:39 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-
 import dotenv from "dotenv";
 dotenv.config();  
 
 import connectDB from "./db/db.js";  
+import { app } from "./app.js";
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port:${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("DB connection failed: ",err);
+});
 
 
 
